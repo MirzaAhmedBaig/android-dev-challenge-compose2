c8668d91e5ef3966205d69044492aa939f01224d
mirza <mirzaahmed65@gmail.com>
2021-03-09 19:11:39 +0530

    applies spotless klint rules

--- app/src/main/java/com/example/androiddevchallenge/ui/components/StateDemoScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/components/StateDemoScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.components
 
 import androidx.compose.foundation.layout.Column
@@ -13,16 +28,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.viewmodel.DemoViewModel
 
-
 @Composable
 fun StateDemoScreen(viewModel: DemoViewModel) {
-    val name:String by viewModel.name.observeAsState("")
+    val name: String by viewModel.name.observeAsState("")
     Scaffold(topBar = { AppBar(appBarText = "Countdown timer") }) {
-        HelloContent(name = name, onValueChange = {
-            viewModel.onNameChange(it)
-        })
+        HelloContent(
+            name = name,
+            onValueChange = {
+                viewModel.onNameChange(it)
+            }
+        )
     }
-
 }
 
 @Composable
--- app/src/main/java/com/example/androiddevchallenge/viewmodel/TimerViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/viewmodel/TimerViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.viewmodel
 
 import android.text.format.DateUtils
@@ -8,7 +23,6 @@ import androidx.lifecycle.ViewModel
 class TimerViewModel : ViewModel() {
     val millisInFuture = DateUtils.MINUTE_IN_MILLIS / 2
     val countdownInterval = DateUtils.SECOND_IN_MILLIS
-
 
     private val _millisUntilFinished = MutableLiveData(millisInFuture)
     val millisUntilFinished: LiveData<Long> = _millisUntilFinished
